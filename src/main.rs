a02dc1c4ea239e232b73676dc0bd542f5d9942c7
Meek Msaki <meek10x@gmail.com>
2023-12-13 13:53:16 -0600

    show addition, and modulo inverse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use ark_ff::{Field, PrimeField, Fp2, BigInteger};
+use ark_bls12_381::Fq as F;
+use ark_std::{One, Zero, UniformRand, test_rng};
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = ark_std::test_rng();
+
+    // select random value from the field
+    let a = F::rand(&mut rng);
+
+    let modulus = <F as PrimeField>::MODULUS;
+
+    // show that 1 + 1 = 2
+    let sum = F::one() + F::one();
+    assert_eq!(sum, F::one().double());
+
+    
+    // show that the multiplicative inverse of a number multiplied by itself equals one.
+    assert_eq!(a * a.inverse().unwrap(), F::one());
+
+    // show that a value raised to the power of the modulus is equal to itself
+    // use the `pow` function to raise to a power
+    assert_eq!(a.pow(modulus), a);
 }
